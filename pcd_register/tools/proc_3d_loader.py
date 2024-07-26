7e0cedae31d6914de14deafd615958c160cbd4db
MyoungchulK <kmc8907@gmail.com>
2024-07-26 21:17:04 +0900

    covar mtx and approx cuvr

--- pcd_register/tools/proc_3d_loader.py
+++ pcd_register/tools/proc_3d_loader.py
@@ -2,69 +2,51 @@ import numpy as np
 
 class proc_3d_loader:
 
-    def __init__(self, verbose = False, use_debug = False):
+    def __init__(self, verbose = False, use_debug = False, use_KDTree = False):
 
         self.verbose = verbose
         self.use_debug = use_debug
+        self.use_KDTree = use_KDTree
+        if self.use_KDTree:
+            from scipy.spatial import KDTree
 
     def get_covar_mtx(self):
 
         pts_nei_cen = np.nanmean(self.pts_nei, axis = 0)
-        self.pts_cov = np.cov(self.pts_nei - pts_nei_cen)
-        self.pts_cov = np.dot(pts_nei_cen.T, pts_nei_cen) / (len(self.pts_nei)-1)
+        self.pts_nei_cov = np.cov((self.pts_nei - pts_nei_cen).T)
         if self.use_debug:
             self.pts_nei_cen = pts_nei_cen
         del pts_nei_cen      
-        print(self.pts_cov)
  
-    def get_appro_curv(self):
+    def get_approx_curv(self):
 
-        eig_val, _ = np.linalg.eigh(self.pts_cov)
-        print(eig_val)
-        print(eig_val.shape)
+        eig_val_nei = np.linalg.eigh(self.pts_nei_cov)[0]
+        self.pts_nei_curv = np.nanmin(eig_val_nei) / np.nansum(eig_val_nei)
+        if self.use_debug:
+            self.eig_val_nei = eig_val_nei
         del eig_val
 
-    def get_3d_process(self, pts, idx, rad):
+    def get_3d_process(self, pts, idx, rad, use_KDTree = False):
 
         pts_i = pts[idx]
-        rad_all = np.sqrt(np.nansum((pts - pts_i[np.newaxis, :]) ** 2, axis = 1))
-        if self.verbose:
-            medi_rad = np.nanmedian(rad_all)
-            print(f'Just you know the median of all radius is {np.round(medi_rad, 2)}')
-            del medi_rad
+        if use_KDTree:
+            rad_nei_idx = tree.query_ball_point(pts_i, rad)
+        else:
+            rad_all = np.sqrt(np.nansum((pts - pts_i[np.newaxis, :]) ** 2, axis = 1))
+            rad_nei_idx = rad_all <= rad
+            if self.use_debug:
+                self.rad_all = rad_all
+            if self.verbose:
+                print(f'Just you know the median of all radius is {np.round(np.nanmedian(rad_all), 2)}')
+            del rad_all
 
-        self.pts_nei = pts[rad_all <= rad]
+        self.pts_nei = pts[rad_nei_idx]
         if self.use_debug:
             self.pts_i = pts_i
-            self.rad_all = rad_all
-        del pts_i, rad_all
+            self.rad_nei_idx = rad_nei_idx
+        del pts_i, rad_nei_idx
 
         self.get_covar_mtx()
 
-        #self.get_appro_curv()
+        self.get_appro_curv()
 
-
-def compute_curvature(pcd, radius=0.5):
-
-    points = np.asarray(pcd.points)
-
-    from scipy.spatial import KDTree
-    tree = KDTree(points)
-
-    curvature = [ 0 ] * points.shape[0]
-
-    for index, point in enumerate(points):
-        indices = tree.query_ball_point(point, radius)
-
-        # local covariance
-        M = np.array([ points[i] for i in indices ]).T
-        M = np.cov(M)
-
-        # eigen decomposition
-        V, E = np.linalg.eig(M)
-        # h3 < h2 < h1
-        h1, h2, h3 = V
-
-        curvature[index] = h3 / (h1 + h2 + h3)
-
-    return curvature
--- pcd_register/wrappers/proc_3d_wrappers.py
+++ pcd_register/wrappers/proc_3d_wrappers.py
@@ -11,8 +11,8 @@ from tools.proc_3d_loader import proc_3d_loader
 @click.command()
 @click.option('-d', '--data', default = '', type = str)
 @click.option('-i', '--index', default = 0, type = int)
-@click.option('-r', '--radius', default = 100, type = float)
-@click.option('-v', '--verbose', default = False, type = bool)
+@click.option('-r', '--radius', default = 0.1, type = float)
+@click.option('-v', '--verbose', default = True, type = bool)
 @click.option('-u', '--use_debug', default = False, type = bool)
 def main(data, index, radius, verbose, use_debug):
 
@@ -20,8 +20,14 @@ def main(data, index, radius, verbose, use_debug):
     pcd.get_pts_in_np()
     pts = pcd.pts
   
-    proc_3d = proc_3d_loader(verbose = verbose, use_debug = use_debug) 
+    proc_3d = proc_3d_loader(verbose = verbose, use_debug = use_debug, use_KDTree = False) 
     proc_3d.get_3d_process(pts, index, radius)  
+    covar_mtx = proc_3d.pts_nei_cov
+    approx_curv = proc_3d.pts_nei_curv
+    
+    print(f'Covariance matrix @ index #{index}: {covar_mtx}')
+    print(f'Approximate Curvature @ index #{index}: {approx_curv}')
+    print(f'Projection of points to the plane  @ index #{index}: {}')
 
 if __name__ == "__main__":
 
